6597efd91ccd93dd887d6d2af64c93bedc1d80ea
James Z.M. Gao <gaozhiming@360.cn>
2019-01-10 23:50:07 +0800

    user action_plugins for save context

---
+++ action_plugins/current_play_context_known_hosts.py
@@ -0,0 +1,25 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.plugins.action import ActionBase
+
+class ActionModule(ActionBase):
+
+    TRANSFERS_FILES = False
+
+    def run(self, tmp=None, task_vars=None):
+        if task_vars is None:
+            task_vars = dict()
+
+        result = super(ActionModule, self).run(tmp, task_vars)
+        del tmp  # tmp no longer has any effect
+
+        ctx = self._play_context
+
+        # connection common
+        result['connection'] = ctx.connection
+        result['remote_addr'] = ctx.remote_addr
+        result['port'] = ctx.port
+
+        result['changed'] = False
+        return result
